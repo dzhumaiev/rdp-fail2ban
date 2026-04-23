13496827bd3b64dc2d28639b5ee13fd3a7abf2d2
Ruslan Dzhumaiev <61501416+dzhumaiev@users.noreply.github.com>
2026-04-23 17:00:59 +0200

    Add files via upload

---
+++ cleanup_logs.ps1
@@ -0,0 +1,8 @@
+﻿$LogDir = 'C:\Program Files (x86)\rdp_guard\logs'
+$limit = (Get-Date).AddDays(-30)
+$logFiles = Get-ChildItem -Path $LogDir -File -Recurse | Where-Object { $_.LastWriteTime -lt $limit }
+foreach ($file in $logFiles) {
+    try {
+        Remove-Item -Path $file.FullName -Force
+    } catch {}
+}
